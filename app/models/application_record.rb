0d35bc3bb1722a655408cfc9744890e9837226bd
Encore Shao <encore@ekohe.com>
2020-02-25 08:56:42 +0800

    MISC - Use i18n config the enum field

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,8 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.human_enumerize_name(enum_name, enum_value)
+    I18n.t("enumerize.#{model_name.i18n_key}.#{enum_name.to_s}.#{enum_value}")
+  end
 end
