556934b5febc1ff5f304ba7d3fc367575728cbc6
Encore Shao <encore@ekohe.com>
2020-03-01 14:26:25 +0800

    MISC - Fixing CSS issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -90,4 +90,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.read_encrypted_secrets = true
+
+  config.action_mailer.default_url_options = { host: "erp.icmoc.com" }
 end
