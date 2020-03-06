e0712bb58ffcc87d0a68bb827fa6e53d0d8cf38e
Encore Shao <encore@ekohe.com>
2020-03-06 22:59:31 +0800

    MISC - Should be store status of layout

--- app/assets/javascripts/actions.js
+++ app/assets/javascripts/actions.js
@@ -475,6 +475,31 @@ function x_navigation_minimize(action) {
 
 }
 
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  document.cookie = name + '=; Max-Age=-99999999;';
+}
+
 function x_navigation() {
 
   $(".x-navigation-control").click(function() {
@@ -489,12 +514,20 @@ function x_navigation() {
     x_navigation_minimize("close");
   }
 
+  var x = getCookie('icmocerplayout');
+  if (x) {
+    $(".page-container").addClass("page-navigation-toggled");
+    x_navigation_minimize("close");
+  }
+
   $(".x-navigation-minimize").click(function() {
 
     if ($(".page-sidebar .x-navigation").hasClass("x-navigation-minimized")) {
+      eraseCookie('icmocerplayout')
       $(".page-container").removeClass("page-navigation-toggled");
       x_navigation_minimize("open");
     } else {
+      setCookie('icmocerplayout', 'expand', 7);
       $(".page-container").addClass("page-navigation-toggled");
       x_navigation_minimize("close");
     }
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,15 +2,10 @@
 
 class ProductsController < BaseController
   def create
-    resource = Product.new(product_params)
-
     create! { collection_path }
   end
 
   def update
-    resource = Product.find params[:id]
-    resource.update_attributes(product_params)
-
     update! { collection_path }
   end
 
@@ -20,9 +15,5 @@ class ProductsController < BaseController
     @products ||= end_of_association_chain.with_name(params[:name])
                                           .includes(%i[brand product_unit])
                                           .page(params[:page]).per(10)
-  end
-
-  def product_params
-    params.require(:product).permit(%i[name model brand_id product_unit_id])
   end
 end
